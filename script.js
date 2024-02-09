b6e2bf7d7329b0db947de8f88fe6c3da8121d56b
maciejkwiatkowski <maciek.kwiatkowski@outlook.com>
2024-02-09 19:49:13 +0100

    Add function to call math function based on operator

--- script.js
+++ script.js
@@ -17,3 +17,16 @@ function divide(a, b) {
 let firstNumber;
 let operator;
 let secondNumber;
+
+function operate(operator, firstNumber, secondNumber) {
+  switch (operator) {
+    case '+':
+      return add(firstNumber, secondNumber);
+    case '-':
+      return subtract(firstNumber, secondNumber);
+    case '*':
+      return multiply(firstNumber, secondNumber);
+    case '/':
+      return divide(firstNumber, secondNumber);
+  }
+}
